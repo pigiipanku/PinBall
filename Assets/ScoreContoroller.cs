f28b402a7b20207b17b225069202e17762364816
早川　尚吾 <pigiipanku@gmail.com>
2017-09-17 19:02:06 +0900

    Lesson5 PinBall
    課題はプロセスはだいたい理解できたのですが、コードが良くわかりませんでした。

---
+++ Assets/ScoreContoroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreContoroller : MonoBehaviour {
+
+    private GameObject score;
+    float sss=0;
+	void Start () {
+        this.score = GameObject.Find("Score");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.score.GetComponent<Text>().text = string.Format("{0}", sss);
+       
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (tag == "SmallStarTag")
+        {
+            sss += 810;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            sss += 1919;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            sss += 114514;
+        }
+    }
+}
+
